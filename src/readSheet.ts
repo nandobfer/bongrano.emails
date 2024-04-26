fbf2a1cb97afa6a1f02d8a4bb66d47b0938d69d6
nandobfer <nandobfer@yahoo.com.br>
2024-04-26 10:20:44 -0300

    feat: sending mails

--- src/readSheet.ts
+++ src/readSheet.ts
@@ -22,7 +22,22 @@ export const readSheet = async () => {
             const data: WhatsappForm = {
                 name: person.name,
                 number: person.phone,
-                template: 'hello_world'
+                template: "mensagem_bongrano_dia_das_maes_1",
+                language: "pt_BR",
+                components: [
+                    {
+                        type: "header",
+                        parameters: [
+                            {
+                                type: "image",
+                                image: {
+                                    // id: "959771299128082", // bozletrando
+                                    id: "967000851500376", // bongrano
+                                },
+                            },
+                        ],
+                    },
+                ],
             }
 
             await api.post('/api/whatsapp', data)
--- types/WhatsappForm.d.ts
+++ types/WhatsappForm.d.ts
@@ -2,4 +2,40 @@ export interface WhatsappForm {
     number: string
     name: string
     template: string
+    language: "en_US" | "pt_BR"
+    components?: WhatsappTemplateComponent[]
+}
+
+export interface WhatsappMedia {
+    id?: string
+    link?: string
+    caption?: string
+}
+
+export interface WhatsappTemplateParams {
+    type: "text" | "currency" | "date_time" | "image" | "document" | "video"
+    text?: string
+    currency?: string
+    date_time?: string
+    image?: WhatsappMedia
+    video?: WhatsappMedia
+    document?: WhatsappMedia
+}
+
+export interface WhatsappTemplateComponent {
+    type: "header" | "body" | "footer"
+    parameters: WhatsappTemplateParams[]
+}
+
+export interface WhatsappApiForm {
+    messaging_product: "whatsapp"
+    to: string
+    type: "template"
+    template: {
+        name: string
+        language: {
+            code: "en_US" | "pt_BR"
+        }
+        components: WhatsappTemplateComponent[]
+    }
 }
